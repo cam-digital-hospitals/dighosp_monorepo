eb445544ac64a43582868d69ffcd6387e6d8d81f
Yin-Chi Chan <ycc39@cam.ac.uk>
2024-07-20 10:00:16 +0100

    Additional aliases for scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -2,3 +2,7 @@
 alias docker-restart="pushd `git root` && docker compose up -d --build && popd"
 alias docker-down="pushd `git root` && docker compose down && popd"
 alias dprune="docker system prune --volumes"
+
+alias gitroot='cd `git root`'
+alias ppy="poetry run python"
+alias plint="poetry run pylint --rcfile=`git root`/.pylintrc"
